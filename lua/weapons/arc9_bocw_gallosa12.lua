b2816a9bca2db1f02ce041cbd14205b1540e5c02
multinett <72457258+multinettt@users.noreply.github.com>
2025-12-06 18:41:57 +0300

    Integrate Options

--- lua/weapons/arc9_bocw_gallosa12.lua
+++ lua/weapons/arc9_bocw_gallosa12.lua
@@ -1,13 +1,13 @@
 --=============================================================================
---   CALL OF DUTY: BLACK OPS COLD WAR
---   Gallo SA12
---   LUA - MAIN
+--   ARC9 CALL OF DUTY: BLACK OPS COLD WAR
+--   SHOTGUNS
+--   GALLO SA12
 --=============================================================================
 --[[
 --   SWEP INFORMATION:
 
 --   BASE  : ARC9
---   BUILD : v2025.7
+--   BUILD : v2025.12
 --   SR.NO : SA9094
 
 
@@ -248,7 +248,15 @@ SWEP.ShootWhileSprint = false
 SWEP.Speed = 1
 
 SWEP.SpeedMult = 1
-SWEP.SpeedMultSprint = 0.706
+
+local SprintMultCvar = GetConVar("arc9_bocw_sprintmultiplier")
+
+if SprintMultCvar:GetBool() then
+    SWEP.SpeedMultSprint = 0.6374269005847955 -- SPRINT SPEED RELATIVE TO MOVE SPEED
+else
+    SWEP.SpeedMultSprint = 1 -- this may possibly change later
+end
+
 SWEP.SpeedMultSights = 0.8
 SWEP.SpeedMultShooting = 0.85
 SWEP.SpeedMultMelee = 0.8
@@ -660,6 +668,22 @@ SWEP.Attachments = {
         CosmeticOnly = true,
     },
 }
+--=========================================================
+-- CUSTOM SLOT INSERTION
+local CustomSlotCvar = GetConVar("arc9_bocw_customslot")
+local CustomSlotTable = {
+    PrintName = "Custom Slot",
+    Bone = "tag_stock",
+    Pos = Vector(0, 0, 0),
+    Ang = Angle(0, 0, 0),
+    Icon_Offset = Vector(-2, 0, 0),
+    Category = {"bocw_custom_slot", "bocw_custom_slot_gallosa12"},
+}
+
+if CustomSlotCvar:GetBool() then
+    table.insert(SWEP.Attachments, CustomSlotTable)
+end
+--=========================================================
 
 SWEP.Hook_ModifyBodygroups = function(self, data)
 
